b9bf8877178b0ed72b8c26ee050ac8adc73058dd
wmctavish <wbmctavish@gmail.com>
2019-04-22 13:28:08 -0400

    minor changes

--- synth.js
+++ synth.js
@@ -1,12 +1,12 @@
 const synth = new Tone.Synth({
-    "oscillator"  : {
-        "type"  : "sawtooth"
-        }  ,
-        "envelope"  : {
-        "attack"  : 0.8,
-        "decay"  : 0.5 ,
-        "sustain"  : 0.4 ,
-        "release"  : 1
+    "oscillator": {
+        "type": "sawtooth"
+        },
+    "envelope": {
+        "attack": 0.01,
+        "decay": 0.1,
+        "sustain": 0.5,
+        "release": 1
         }
 });
 
@@ -16,10 +16,10 @@ const reverb = new Tone.Reverb({
 });
 reverb.toMaster();
 
-const filter = new Tone.Filter(900, "lowpass");
+const filter = new Tone.Filter(500, "lowpass");
 filter.connect(reverb);
 
-const gain = new Tone.Gain(0.02);
+const gain = new Tone.Gain(0.05);
 gain.connect(filter);
 
 synth.connect(gain);
@@ -28,7 +28,7 @@ const rows = document.body.querySelectorAll('div > div'),
     notes = ['C4', 'B3', 'A#3', 'A3', 'G#3', 'G3', 'F#3', 'F3', 'E3', 'D#3', 'D3', 'C#3', 'C3'];
 let index = 0;
 
-Tone.Transport.scheduleRepeat(repeat, '8n');
+Tone.Transport.scheduleRepeat(repeat, '16n');
 Tone.Transport.start();
 
 function repeat(time) {
@@ -37,7 +37,7 @@ function repeat(time) {
         let note = notes[i],
         row = rows[i],
         input = row.querySelector(`input:nth-child(${step + 1})`);
-    if (input.checked) synth.triggerAttackRelease(note, '8n', time);
+    if (input.checked) synth.triggerAttackRelease(note, '16n', time);
     }
     index++;
 }
