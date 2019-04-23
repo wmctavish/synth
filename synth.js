64708a810b1ae501513a4961c17b6601774c1be7
William McTavish <wbmctavish@gmail.com>
2019-04-23 18:56:34 -0400

    Styled slightly

--- synth.js
+++ synth.js
@@ -1,3 +1,4 @@
+
 const synth1 = new Tone.Synth({
     "oscillator": {
         "type": "sawtooth"
@@ -57,9 +58,10 @@ const synth2 = new Tone.Synth({
     synth2.connect(gain);
 
 //16-Step Sequencer
-const rows = document.body.querySelectorAll('div > div'),
+const rows = document.body.querySelectorAll('.step-sequencer > div'),
     octave = ['C5', 'B4', 'A#4', 'A4', 'G#4', 'G4', 'F#4', 'F4', 'E4', 'D#4', 'D4', 'C#4', 'C4', 'B3', 'A#3', 'A3', 'G#3', 'G3', 'F#3', 'F3', 'E3', 'D#3', 'D3', 'C#3', 'C3', 'B2', 'A#2', 'A2', 'G#2', 'G2', 'F#2', 'F2', 'E2', 'D#2', 'D2', 'C#2', 'C2']
 let index = 0;
+
 
 let bpm = 120;
 Tone.Transport.scheduleRepeat(repeat, '8n');
@@ -67,16 +69,16 @@ Tone.Transport.start();
 
 
 function repeat(time) {
-    let step = index % 16;
+    let step = index % 32;
     for(i=0;i<rows.length;i++) {
         let note = octave[i],
         row = rows[i],
-        input = row.querySelector(`input:nth-child(${step + 1})`);
+        input = row.querySelector(`input:nth-child(${step + 2})`);
     if (input.checked) 
         synth1.triggerAttackRelease(note, '8n', time),
         synth2.triggerAttackRelease(note, '8n', time);
     }
-    index++;
+    index += 2;
 };
 
 //Global envelop controls
